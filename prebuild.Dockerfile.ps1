582505bdc805c761be82a73c387363c978028eb6
Alex2021cyberhuman <alex2021cyberhuman@gmail.com>
2022-02-06 23:29:28 +0500

    feat: Add git files and other misc files

---
+++ prebuild.Dockerfile.ps1
@@ -0,0 +1,9 @@
+﻿param(
+  [String]$Config = 'DEBUG',
+  [String]$RuntimeIdentifier = 'linux-x64',
+  [String]$Tag = 'likes:prebuild',
+  [String]$PathToRepository = './'
+)
+$Publish = "$( $PathToRepository )app/publish/"
+dotnet publish "$( $PathToRepository )Conduit.Comments.WebApi" -c $CONFIG -o $Publish -r $RuntimeIdentifier --no-self-contained
+docker build -t $tag -f "$( $PathToRepository )prebuild.Dockerfile" $PathToRepository
